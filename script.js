bad04222c8ec18de835982c29c1e4be5eaf6ad14
nipun1a <nipun1a1b@gmail.com>
2025-05-18 09:12:40 +0530

    practice purpouse

---
+++ script.js
@@ -0,0 +1,6 @@
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),// Initialize LocomotiveScroll AND put the main div in this document query selector
+    smooth: true,
+    lerp: 0.02
+});
